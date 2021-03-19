5ea6a13acec18dcede5e5b0174a80f20faeb2b11
fabrizio <faber.rvn@gmail.com>
2021-03-19 13:17:15 +0100

    #1 Project Setup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# react-shopping-cart
+# React shopping cart
 A demo app to learn the basics of React and to create a shopping cart using classes.
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<h1>Start the project</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
